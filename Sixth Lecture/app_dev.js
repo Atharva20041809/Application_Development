165229a05aa028e7fefd26f206a21db9d4ca8b4e
Atharva Tiwari <atharvatiwari@atharvas-MacBook-Pro-2.local>
2026-08-26 12:54:27 +0530

    sixth lecture

---
+++ Sixth Lecture/app_dev.js
@@ -0,0 +1,128 @@
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { spawn } = require("child_process");
+// const { get } = require("http");
+// const { dir, time } = require("console");
+process.stdin.setRawMode(true);
+
+let user_input = 0;
+let songs = undefined;
+let player = undefined;
+let song_is_playing = false;
+let duration = undefined;
+let timeElapsed = undefined;
+
+async function getSongDuration(directoryPath) {
+  return new Promise((res, rej) => {
+    let get_duration = spawn("afinfo", [directoryPath]);
+    get_duration.stdout.on("data", (data) => {
+      res(
+        Number(data.toString().split("estimated duration: ")[1].split(".")[0]),
+      );
+    });
+  });
+}
+
+function progressBarLoader(duration) {
+  timeElapsed = 0;
+  setInterval(() => {
+    if (song_is_playing && player) {
+      timeElapsed += 0.1;
+    }
+    listSongs(join("songs"));
+  }, 100);
+}
+
+function listSongs(directoryPath) {
+  songs = readdirSync(directoryPath);
+  process.stdout.write("\x1B[2J");
+  process.stdout.write("\x1B[2;1H");
+
+  let menuText = songs
+    .map((ele, ind) => {
+      if (user_input == ind) {
+        return `> ${ele}`;
+      } else {
+        return `${ele}`;
+      }
+    })
+    .join("\n");
+  process.stdout.write(menuText);
+  let percentage = Math.round((timeElapsed / duration) * 100);
+  if (song_is_playing) {
+    process.stdout.write(
+      `\n${timeElapsed.toFixed(2)} / ${duration} : ${percentage}`,
+    );
+  }
+}
+
+async function playSongs(directoryPath) {
+  player = spawn("vlc", ["--intf", "rc", directoryPath], { stdio: "pipe" });
+  duration = await getSongDuration(directoryPath);
+  progressBarLoader(duration);
+  //   console.log(`\n${duration}`);
+  // console.log(player)
+}
+
+listSongs(join("songs"));
+
+process.stdin.on("data", (data) => {
+  //   console.log(data);
+  if (data[0] == 0x0d) {
+    if (!song_is_playing) {
+      playSongs(join("songs", songs[user_input]));
+      song_is_playing = true;
+    } else {
+      player.stdin.write("quit\n");
+      process.exit(0);
+      song_is_playing = false;
+    }
+    return;
+  }
+  if (data[0] == 3) {
+    process.exit(0);
+    return;
+  }
+  if (data[0] == 0x20) {
+    // player.kill('SIGSTOP')
+    // player.kill('SIGKILL')
+    // player.('SIGTERM')
+    // player('SIGINT')
+    if (song_is_playing) {
+      player.stdin.write("pause\n");
+      song_is_playing = false;
+    } else {
+      player.stdin.write("pause\n");
+      song_is_playing = true;
+    }
+  }
+  if (data[0] == "0x6E") {
+    player.stdin.write("pause\n");
+    user_input = Math.min(songs.length - 1, user_input + 1);
+    listSongs(join("songs"));
+    playSongs(join("songs", songs[user_input]));
+  }
+  if (data[0] == "0x64") {
+    player.stdin.write("pause\n");
+    user_input = Math.max(0, user_input - 1);
+    listSongs(join("songs"));
+    playSongs(join("songs", songs[user_input]));
+  }
+  if (data[0] == 0x1b && data[1] == 0x5b) {
+    if (data[2] == 0x41) {
+      user_input = Math.max(0, user_input - 1);
+      listSongs(join("songs"));
+    }
+    if (data[2] == 0x42) {
+      user_input = Math.min(songs.length - 1, user_input + 1);
+      listSongs(join("songs"));
+    }
+  }
+  //   listSongs(join("songs"));
+});
+
+//afinfo
+
+// \r\x1B[0k
+
+// \n\x1B[0k
